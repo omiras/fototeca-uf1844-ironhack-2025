09c45a50c5415aee76138909a1db9cd19ffe66e7
omiras <oscar.miras@gmail.com>
2025-05-19 20:57:01 +0200

    done

--- index.js
+++ index.js
@@ -10,10 +10,24 @@ app.use(express.urlencoded({ extended: true}));
 // Oye, si te hacen una petición GET, mira si tienes alguno de los recursos que te piden en el directorio 'public'
 app.use(express.static('public'));
 
+// Array de fotos (base de datos actual)
+const photos = [];
+
 app.get("/", (req, res)=>{
     res.send("funciona");
 });
 
+app.get("/new-image", (req, res)=>{
+  // 1. Teneis que renderizar una vista new-image.ejs donde aparezca un formulario para que el usuario rellene los datos que pide la práctica. 
+  // Yo implementaré el título y la URL
+
+});
+
+// Necesitamos un endpoint donde enviar los datos del formulario. Endpoint donde enviar los datos del formulario
+app.post("/new-image", (req, res)=>{
+  // Recibir los datos del formulario y actualizar el array "photos"
+  // Para comprobar que lo has hecho bien muestra por consola el contenido del array photos después de actualizarlo
+});
 
 // Iniciar el servidor
 app.listen(PORT, () => {
