6c92c427207884639558782abff171f87520fbb9
omiras <oscar.miras@gmail.com>
2025-05-20 19:14:35 +0200

    done

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 // Array de fotos (base de datos actual)
-const photos = [];
+const images = [];
 
 app.get("/", (req, res)=>{
-    res.send("funciona");
+    res.render("home.ejs", {
+      images : images
+    });
 });
 
 app.get("/new-image", (req, res)=>{
@@ -26,16 +28,16 @@ app.get("/new-image", (req, res)=>{
 
 // Necesitamos un endpoint donde enviar los datos del formulario. Endpoint donde enviar los datos del formulario
 app.post("/new-image", (req, res)=>{
-  // Recibir los datos del formulario y actualizar el array "photos"
-  // Para comprobar que lo has hecho bien muestra por consola el contenido del array photos después de actualizarlo
+  // Recibir los datos del formulario y actualizar el array "images"
+  // Para comprobar que lo has hecho bien muestra por consola el contenido del array images después de actualizarlo
   //  Disponemos de un objeto en req.body donde tendremos todos los valores
   // de la petición POST (lo que nos viene del formulario)
-  photos.push({
+  images.push({
     title: req.body.title,
     url: req.body.url
   });
 
-  console.log("Array de fotos actualizado: ", photos);
+  console.log("Array de imágenes actualizado: ", images);
 
   res.send("Imagen añadida correctamente a la galería.");
 });
