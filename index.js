e3bce7595dac416cf68ed4f4a2dde88a6ee8b6fd
omiras <oscar.miras@gmail.com>
2025-06-17 19:32:52 +0200

    Add mongoose integration and update image handling
    - Implemented mongoose for MongoDB connection and image schema.
    - Updated routes to handle image retrieval and addition.
    - Enhanced form validation for image title input.

--- index.js
+++ index.js
@@ -1,31 +1,67 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
 
 const app = express();
 const PORT = 3000;
 
 // Nos permite procesar peticiones POST que vengan de un formulario
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Oye, si te hacen una petición GET, mira si tienes alguno de los recursos que te piden en el directorio 'public'
 app.use(express.static('public'));
 
 // Usamos el middleware morgan para registrar todas las peticiones que vienen del cliente
-app.use(morgan("dev")); 
+app.use(morgan("dev"));
 
-// Array de fotos (base de datos actual)
-const images = [];
+// Modelo Image
+let Image;
+
+async function start() {
+  await mongoose.connect('mongodb+srv://oscar:oscar@cluster0.c8tq0vp.mongodb.net/ironhackDB-2025');
+  // Iniciar el servidor
+
+  const imageSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      maxlength: 15,
+      required: true
+    },
+    url: {
+      type: String,
+      required: true,
+      match: /^(https?:\/\/)[^\s$.?#].[^\s]*$/i
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    dominantColor: String
+  })
+
+  Image = mongoose.model('Images', imageSchema);
 
 
-app.get("/", (req, res)=>{
-    res.render("home.ejs", {
-      images : images
-    });
+  app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  });
+
+}
+
+app.get("/", async (req, res) => {
+
+  // Recuperar todas las imágenes del a base de datos
+  const images = await Image.find();
+
+  res.render("home.ejs", {
+    images: images
+  });
 });
 
 
-app.get("/new-image", (req, res)=>{
+app.get("/new-image", (req, res) => {
   // 1. Teneis que renderizar una vista new-image.ejs donde aparezca un formulario para que el usuario rellene los datos que pide la práctica. 
   // Yo implementaré el título y la URL
   res.render("add-image.ejs", {
@@ -35,25 +71,32 @@ app.get("/new-image", (req, res)=>{
 });
 
 // Necesitamos un endpoint donde enviar los datos del formulario. Endpoint donde enviar los datos del formulario
-app.post("/new-image", (req, res)=>{
+app.post("/new-image", async (req, res) => {
   // Recibir los datos del formulario y actualizar el array "images"
   // Para comprobar que lo has hecho bien muestra por consola el contenido del array images después de actualizarlo
   //  Disponemos de un objeto en req.body donde tendremos todos los valores
   // de la petición POST (lo que nos viene del formulario)
 
   // Usar el módul de terceros get-dominant-color para obtener el color (o colores) predominantes de la imagen y lo guardas en una variable
-  
+
   let dominantColor;
   // dominantColor = get-dominant-color(req.body.url)
-
-  images.push({
+  const newDocument = new Image({
     title: req.body.title,
     url: req.body.url,
-    dominantColor: dominantColor
+    date: new Date()
   });
+  try {
+    await newDocument.save();
+  } catch (err) {
+    // Fallar gracilmente
+    // Informar a los desarrolladores que algo ha ido mal
+    console.log(err.message);
 
-  console.log("Array de imágenes actualizado: ", images);
-
+    return res.render("add-image.ejs", {
+      message: "No hemos podido insertar su imagen en este momento...pruebe con otra URL"
+    })
+  }
   res.render("add-image.ejs", {
     message: "La imagen se ha añadido correctamente"
   });
@@ -66,7 +109,6 @@ app.use((req, res) => {
   res.status(404).render('404.ejs');
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+start().catch(err => console.log(err));
+
+
